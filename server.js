83d80b49767df1a4868731c0bba951bc77cf1e20
tracybrtn <tracynburton@gmail.com>
2022-10-14 12:03:28 -0500

    Add Heroku

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const { animals } = require('./data/animals');
+
+app.listen(3001, () => {
+    console.log('API server now on port 3001!');
+});
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        //Save personalityTraits as a dedicated array
+        //If personalityTraits is a string, place it into a new array and save
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits]
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        //Loop through each trait in the personalityTraits array:
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
