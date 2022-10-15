057cc0662200b67ae7de8368ce9640d78e328a6f
tracybrtn <tracynburton@gmail.com>
2022-10-15 15:45:04 -0500

    Web server created

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -45,3 +50,12 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+        if (result) {
+        res.json(result);
+        } else {
+            res.send(404);
+        }
+  });
