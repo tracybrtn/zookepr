1c1173ccd7170c54d237d30460e318aa6d77aa92
tracybrtn <tracynburton@gmail.com>
2022-10-16 23:48:12 -0500

    modularized code

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const router = require('express').Router();
+
+//Connecting server with HTML
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+ });
+
+router.get('/animals', (req, res) => {
+res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+module.exports = router;
